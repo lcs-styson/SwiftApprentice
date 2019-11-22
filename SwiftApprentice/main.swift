65ed8c789228ff163d5656b034f4e6adf845c763
Sebastian Tyson <styson@lcs.on.ca>
2019-11-22 14:33:56 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Tyson, Sebastian on 2019-11-22.
+//  Copyright © 2019 Tyson, Sebastian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
